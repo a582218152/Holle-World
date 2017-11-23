fe53c7caa0e5208bdbadbc927356445f6d521c61
SoCallMeJianLai <houyeah@126.com>
2017-11-23 07:44:40 -0800

    add ALLOWED_HOST

--- djangoHelloWorld/djangoHelloWorld/settings.py
+++ djangoHelloWorld/djangoHelloWorld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'um(giejl)#@ebbak^k__yw@^y48(&w=nff45p78u1+af-crefb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.92.128"]
 
 
 # Application definition
